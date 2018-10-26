93422416eb61c457d47e21b5732bb1c1e1734349
Justin Daniel <justinddaniel@gmail.com>
2018-10-26 16:08:40 -0400

    isValidEmailAddress.js now passes tests

--- src/isValidEmailAddress.js
+++ src/isValidEmailAddress.js
@@ -1,3 +1,10 @@
-export const isValidEmailAddress = () => {
-  // TO IMPLEMENT IN ANOTHER PR
+export const isValidEmailAddress = (email) => {
+  try { //first check if email argument provided and of type string
+    if (!email || typeof email !== 'string') throw "you must provide an email of type string";
+  }
+  catch(err) {
+    return err
+  }
+  const pattern = email.match(/\D+@\D+\.\D/) // if email is truthy check regex pattern
+  return pattern ? email : null // if pattern is truthy return email, or else return null 
 };
